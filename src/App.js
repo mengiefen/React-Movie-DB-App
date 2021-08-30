a2c222588df53edc6b65f9277596378fc1898bed
mengiefen <mengiefen@gmail.com>
2021-08-30 16:06:15 +0300

    Finished Move DB React App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
-//Components
+// Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Components
 import Header from './components/Header';
 import Home from './components/Home';
-
-//Styles
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
+import Login from './components/Login';
+// Context
+import UserProvider from './context';
+// Styles
 import { GlobalStyle } from './GlobalStyle';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => (
+  <Router>
+    <UserProvider>
       <Header />
-      <Home />    
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/:movieId' element={<Movie />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
       <GlobalStyle />
-    </div>
-  );
-}
-//This part is about react implementation...
+    </UserProvider>
+  </Router>
+);
 
 export default App;
